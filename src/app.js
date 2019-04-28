6c8b6d47063d4c33941e6b997751b0029cac996a
Opender Singh <opender94@gmail.com>
2019-04-28 18:20:41 +1200

    Updated lib

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import orderedJSONParse from "../lib/orderedJSONParse";
+import orderedJSONStringify from "../lib/orderedJSONStringify";
+
+const desiredOrderString = `{
+    "nested1": {
+      "zebra": [
+        5, 6, 7, { "fruit": "banana" }
+      ],
+      "nested2": {
+        "b": 10,
+        "a": true
+      }
+    },
+    "foo": [1, 2, 3, { "d": "second", "c": "first" }, "test"]
+  }`;
+
+const defaultOrderObject = {
+    "foo": [1, 2, 3, { "c": "first", "d": "second", }, "test",],
+    "nested1": {
+        "nested2": {
+            "a": true,
+            "b": 10,
+        },
+        "zebra": [
+            5, 6, 7, { "fruit": "banana", },
+        ],
+    },
+};
+
+const parsed = orderedJSONParse(desiredOrderString);
+const fixedOrder = orderedJSONStringify(defaultOrderObject, parsed.map);
+
+console.log(`Random order  : ${JSON.stringify(defaultOrderObject)}`);
+console.log(`Desired order : ${desiredOrderString.replace(/\s/g, "")}`);
+console.log(`Fixed order   : ${JSON.stringify(fixedOrder)}`);
+console.log(`Lookup        : ${JSON.stringify(parsed.map)}`);
