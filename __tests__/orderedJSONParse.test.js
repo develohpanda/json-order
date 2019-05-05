f66aa7fac96cba2d8998a331a39c1114a6e72051
Opender Singh <opender94@gmail.com>
2019-05-05 13:52:08 +1200

    Stuff

--- __tests__/orderedJSONParse.test.js
+++ __tests__/orderedJSONParse.test.js
@@ -1,10 +1,12 @@
-import {orderedJSONParse} from "../src/orderedJSONParse";
+// @flow
 
+import orderedJSONParse from "../lib/orderedJSONParse";
+import type {OrderedParseResult} from "../lib/models";
 
 describe("orderedJSONParse ", () => {
     it("returns nothing for a blank JSON string", () => {
-        const result = orderedJSONParse("{}");
+        const result: OrderedParseResult = orderedJSONParse("{}");
 
-        
+        expect(result.result).toBe({});
     });
 })
