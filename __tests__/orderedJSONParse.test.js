2e76d1bd042619e2ab5eae63195e9f32f17fc5a1
Opender Singh <opender94@gmail.com>
2019-05-05 13:34:51 +1200

    added flow-typed

---
+++ __tests__/orderedJSONParse.test.js
@@ -0,0 +1,10 @@
+import {orderedJSONParse} from "../src/orderedJSONParse";
+
+
+describe("orderedJSONParse ", () => {
+    it("returns nothing for a blank JSON string", () => {
+        const result = orderedJSONParse("{}");
+
+        
+    });
+})
